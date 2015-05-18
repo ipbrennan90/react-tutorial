02f2c0a7dcc2ab444c7b795cd4d483eac66fb056
Ian Brennan <ian.brennan@icloud.com>
2015-05-18 00:34:38 -0600

    comments table made

---
+++ db/migrate/20150518062740_create_commentsa.rb
@@ -0,0 +1,6 @@
+class CreateCommentsa < ActiveRecord::Migration
+  def change
+    create_table :commentsas do |t|
+    end
+  end
+end
---
+++ db/migrate/20150518063013_drop_commentsa.rb
@@ -0,0 +1,5 @@
+class DropCommentsa < ActiveRecord::Migration
+  def change
+    drop_table :commentsas
+  end
+end
